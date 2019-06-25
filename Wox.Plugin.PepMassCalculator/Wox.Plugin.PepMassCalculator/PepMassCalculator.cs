67ecf2b1a6f6c9c89a885054b155f1c38147e7a2
g p <o_name@live.de>
2019-06-25 17:30:47 +0200

    removed old and created new project

---
+++ Wox.Plugin.PepMassCalculator/Wox.Plugin.PepMassCalculator/PepMassCalculator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wox.Plugin.PepMassCalculator
+{
+    public class PepMassCalculator : IPlugin
+    {
+        public List<Result> Query(Query query) {
+            throw new NotImplementedException();
+        }
+
+        public void Init(PluginInitContext context) {
+            throw new NotImplementedException();
+        }
+    }
+}
